6423e26af9d4dbfdd9ebc107d81a94b7e608db51
Metts <metts@Mettss-MacBook-Pro.local>
2019-04-13 19:13:50 -0500

    first commit

---
+++ carMuseum.js
@@ -0,0 +1,24 @@
+// console.log("test")
+
+
+let carNumber = 0
+
+const takeNumber = function (carName) {
+    carNumber += 1
+    console.log(carNumber + " " + carName)
+    
+    
+    
+    /*
+        Write your awesome code here. See comments
+        below for what should be returned.
+    */
+}
+
+const Ford = takeNumber("Ford Mustang")
+// console.log(Ford)  // This should print "1. Galactic Scum" in the console
+
+const chevy = takeNumber("Chevy Z-28")
+// console.log(chevy)  // This should print "2. Underdogs" in the console
+
+const dodge = takeNumber("Dodge Charger")
